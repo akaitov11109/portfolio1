9e8e70c14c6bf94598ded788ed0612799037cea3
Astemir <akaitov11111@icloud.com>
2025-03-14 00:10:53 +0300

    Updated

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const burger = document.querySelector(".burger-menu");
     const nav = document.querySelector(".nav-bar");
 
-    burger.addEventListener("click", function () {
+    burger.addEventListener("click", function (e) {
+        e.stopPropagation();  // Останавливаем распространение события, чтобы клик по бургеру не закрывал меню
         nav.classList.toggle("active");
         burger.classList.toggle("active");
+    });
+
+    // Закрытие меню при клике на любое место вне меню и бургера
+    document.addEventListener("click", function (e) {
+        if (!nav.contains(e.target) && !burger.contains(e.target)) {
+            nav.classList.remove("active");
+            burger.classList.remove("active");
+        }
     });
 
     // Закрытие меню после клика на ссылку в меню
@@ -21,20 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (nav.classList.contains("active")) {
             nav.classList.remove("active");
             burger.classList.remove("active");
-        }
-    });
-});
-  
-  //Кнопка для прокрутки
-  
-document.addEventListener("DOMContentLoaded", function () {
-    const scrollToTopButton = document.querySelector(".scroll-to-top");
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 300) {
-            scrollToTopButton.style.display = "block";
-        } else {
-            scrollToTopButton.style.display = "none";
         }
     });
 });
