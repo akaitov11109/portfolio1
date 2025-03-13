9df1ce8639b4feb5961c855fd384a5234e0da278
Astemir <akaitov11111@icloud.com>
2025-03-13 23:07:05 +0300

    Правки

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
         nav.classList.toggle("active");
         burger.classList.toggle("active");
     });
+
+    // Закрытие меню после клика на ссылку в меню
+    const navLinks = document.querySelectorAll(".nav-bar a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            nav.classList.remove("active");
+            burger.classList.remove("active");
+        });
+    });
 });
